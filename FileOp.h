ba723bb90e09a9fb47e646c8920dbd25d76f92e4
CodingGardian <62391363+CodingGardian@users.noreply.github.com>
2020-03-19 12:13:03 -0400

    Update FileOp.h

--- FileOp.h
+++ FileOp.h
@@ -1,9 +1,9 @@
 #pragma once
 
 
-    extern inline void readFile(std::string name, std::vector<char>& vec);
-    extern inline void readAndEraseFile(std::string name, std::vector<char>& vec);
-    extern inline bool fileExist(std::string& name);
-    extern inline bool fileExist(const char* name);
+    extern void readFile(std::string name, std::vector<char>& vec);
+    extern void readAndEraseFile(std::string name, std::vector<char>& vec);
+    extern bool fileExist(std::string& name);
+    extern bool fileExist(const char* name);
 
 
