ac705a1d9abbc2b81ffedaf2ce7a944b5fcd81ff
CodingGardian <62391363+CodingGardian@users.noreply.github.com>
2020-03-19 11:28:39 -0400

    Create FileOp.h

---
+++ FileOp.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+    extern inline void readFile(std::string name, std::vector<char>& vec);
+    extern inline void readAndEraseFile(std::string name, std::vector<char>& vec);
+    extern inline bool fileExist(std::string& name);
+    extern inline bool fileExist(const char* name);
+
+
